57eaa6a766dee8022067bae1fd4640da741b5a90
OMC-NZ <nz.dev@oppomobile.nz>
2024-11-26 16:49:06 +1300

    config package & pm2 environment

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps: [
+        {
+            name: "OPPONZ-Promotions-API-Nodejs",
+            script: "index.js",
+            exec_mode: "cluster",
+            instances: 1, // 实例数（根据需要设置）
+            env: {
+                NODE_ENV: "production", // 生产环境
+            },
+        },
+    ],
+};
